81d2fe91617276646da5790c93977fbada6d8af4
vudpecker <96355168+vudpecker@users.noreply.github.com>
2023-01-31 21:49:07 +0000

    added exercises

---
+++ dtype.py
@@ -0,0 +1,20 @@
+import sys
+
+a = 14
+b = 25
+c = a + b
+d = c % 3
+#print(f"remainder is : {d}")
+
+print("a" + "b") # Prints as string
+
+print(a + b) 
+#Split attribute of string
+print("Hello:Nick:welcome".split(":")[1])
+print('Hello Nick welcome'.split())
+#Array or List
+print(['Hello', 'Nick', 'welcome'][2])
+print(len(['Hello', 'Nick', 'welcome'][2]))
+
+#print python version
+print(sys.version.split()[0])
---
+++ hashtuple.py
@@ -0,0 +1,13 @@
+#Enter range as input
+num = int(input())
+lis = []
+for i in range(num):
+    lis.append(i+1)
+t = tuple(lis)
+print(t)
+print(hash(t))
+
+''' 
+Hash of (1,2) is -3550055125485641917
+Expected is 3713081631934410656
+'''
