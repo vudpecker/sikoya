18d3cbc200148c5fdd7358f4c002bd9819c21a2d
vudpecker <96355168+vudpecker@users.noreply.github.com>
2023-05-31 03:29:44 +0000

    added formated print

--- basic.py
+++ basic.py
@@ -1,3 +1,10 @@
 """Multiple assignment"""
 bob, sera, mike = 19,21,20
 print("Bob's age is " + str(bob))
+print("Mike's age is " + str(mike))
+
+stud_name = "Jake" # pylint: disable=C0103
+message = "%s is 16 yrs old" # pylint: disable=C0103
+print(message%stud_name)
+message="%s is %d yrs old" # pylint: disable=C0103
+print(message%("Phil",18))
--- hashtuple.py
+++ hashtuple.py
@@ -7,7 +7,7 @@ t = tuple(lis)
 print(t)
 print(hash(t))
 
-''' 
+"""
 Hash of (1,2) is -3550055125485641917
 Expected is 3713081631934410656
-'''
+"""
