e66aff5bb084696e337ee948676b72b6294f5c83
Murugaraja <muru@sweethome.com>
2023-06-11 22:34:12 +0200

    added palindrome check

--- sandbox.py
+++ sandbox.py
@@ -4,3 +4,10 @@ thisset = {"apple", "banana", "cherry", True, 1, 2} # pylint: disable=duplicate-
 
 new_set = thisset.copy()
 print(new_set)
+
+string = "liril1"
+
+string = string.lower().replace(" ", "")
+
+if string == string[::-1]:
+    print("Palindrome")
