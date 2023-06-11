d0c89def2e85ba780f7b015293a51420716fcde0
vudpecker <96355168+vudpecker@users.noreply.github.com>
2023-06-11 16:09:06 +0200

    Update basic_list.py
    includes range and enumerate functions

--- basic_list.py
+++ basic_list.py
@@ -11,8 +11,19 @@ print(new_list, end="\n")
 
 album_list = [ 1, "Michael Jackson", "Thriller", 1982, "00:42:19", \
               "Pop, Rock, R&B", 46.0, 65, "30-Nov-82", None, 10.0, True]
-
 #convert to a set to remove duplicates
 album_set = set(album_list)
 
+length = len(album_list)
+
+for i in range(length):
+  print(album_list[i])
+
 print(album_set)
+
+squares=['red', 'yellow', 'green', 'purple', 'blue']
+
+#Use of enumerate instead of range
+for i, square in enumerate(squares):
+    print(i, square)
+    
