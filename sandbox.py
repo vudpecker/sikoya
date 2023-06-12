25381634f5a5f1f81019e87998ffb5107bfc5cf3
Murugaraja <muru@sweethome.com>
2023-06-12 06:56:02 +0200

    added even check

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,5 @@
 #Playground excersices
+from random import randint
 
 thisset = {"apple", "banana", "cherry", True, 1, 2} # pylint: disable=duplicate-value
 
@@ -10,13 +11,10 @@ print(ord("a"))
 WIDTH = 2
 
 if not isinstance(WIDTH,int):
+
     print("It is not a valid input")
 else:
     print("It is a valid input")
-
-print(type(WIDTH))
-
-
 
 string = "liril"
 
@@ -26,3 +24,14 @@ string = string.lower().replace(" ", "")
 "string reverse"
 string1 = string[::-1]
 print(string1)
+
+t = ["Rock", "Paper", "Scissors"]
+computer = t[randint(0,2)]
+#player = input("Rock, Paper, Scissors?")
+#if player == computer:
+#    print("Tie!")
+#print(player)
+
+n = [0, 2, 3, 4,5, 6]
+for i in n:
+    print (i % 2)
