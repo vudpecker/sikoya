e4c2181d2df25cc7a10230189ce1fb8e4512a706
Murugaraja <muru@sweethome.com>
2023-12-26 11:04:50 +0100

    file handling for 1M characters with logging included

--- exercise/filehandling/exerc_file_read.py
+++ exercise/filehandling/exerc_file_read.py
@@ -1,23 +1,36 @@
-"""with exception handling"""
-
+import logging
 from pathlib import Path
 
 
-path =  Path('pi_value.txt') # if relative path ..\..\pi_value.txt possible?
+#path =  Path("\WorkHome\Raaj\PyProject\sikoya\exercise\filehandling\pi_value.txt") # absolute path 
+path =  Path("exercise\\filehandling\pi_value.txt") #relative path
 
-def print_path(path):
-        
+def print_path(path):     
+    """file operations with exception handling"""
 
+    logging.basicConfig(level = "DEBUG")
     try:
-        contents = path.read_text()
+        #contents = path.read_text()
+        contents = path.read_text().rstrip() # to strip trailing newline characters
 
     except FileNotFoundError:
         print(f"The file {path} does not exist")
         #pass #fail silently!
 
-    else:    
-        print(contents.rsplit())
+    else:
 
+        logging.debug(f"The file content is {contents}")
+        #print(contents.rstrip()) #to strip trailing newline characters
+        
+        lines = contents.splitlines()
+        logging.debug(f"The file content is {lines}")
+        pi_string = ""
+        for line in lines:
+            pi_string += line.lstrip()
+        
+        print(pi_string)
+
+        
 if __name__ == '__main__':
-    print("printing the patch...")
+    print("printing the value of pi...")
     print_path(path)
