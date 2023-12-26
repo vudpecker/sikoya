d0edec9b59a29980ee5ae02f79c1f46473c1f74e
Murugaraja <muru@sweethome.com>
2023-12-26 11:06:34 +0100

    follows prev commit

--- exercise/filehandling/exerc_file_read.py
+++ exercise/filehandling/exerc_file_read.py
@@ -1,9 +1,14 @@
 import logging
 from pathlib import Path
 
+choice = input("Do you want to access pi full value?\n")
 
 #path =  Path("\WorkHome\Raaj\PyProject\sikoya\exercise\filehandling\pi_value.txt") # absolute path 
-path =  Path("exercise\\filehandling\pi_value.txt") #relative path
+if choice.lower() == "yes":
+    path = Path("exercise\\filehandling\pi_value_full.txt") #relative path
+else:
+    path =  Path("exercise\\filehandling\pi_value.txt") #relative path
+
 
 def print_path(path):     
     """file operations with exception handling"""
@@ -26,9 +31,13 @@ def print_path(path):
         logging.debug(f"The file content is {lines}")
         pi_string = ""
         for line in lines:
+            line = line.rstrip()
             pi_string += line.lstrip()
         
         print(pi_string)
+
+        if choice:
+            print(f"{pi_string[:52]}...")
 
         
 if __name__ == '__main__':
